27c028663bb70237e8b099fdbb5d71260e9bd6fd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-11 21:58:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == 0
+    "The line is currently empty."  
+  else
+    
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+end
