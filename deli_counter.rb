2ce472ffc248b6692bf20e0f7e9c9d2ace30bbbe
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-11 23:32:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
     puts "The line is currently empty."  
   else
     katz_deli.each.with_index(1) do |name, index|
-    line_array << "#{index}, #{name}."
+    line_array << "#{index}. #{name}."
   end
 end
 
