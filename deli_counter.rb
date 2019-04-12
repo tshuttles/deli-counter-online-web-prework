88be539a383deec16fdd758b0bf47beb61279fe4
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 00:08:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-  else 
+  else katz_deli.length > 0
+    puts "#{name}"
   end  
 end
